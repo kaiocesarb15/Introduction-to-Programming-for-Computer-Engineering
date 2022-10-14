6f83be21a7e6d143c9ed339211309165d9bc354a
Kaio César Barreto <kaio.barreto@academico.ufpb.br>
2022-10-13 22:06:10 -0300

    Upload ex001-helloWorld.c
    This code should return the phrase Hello World!!!, my name is (your name).

---
+++ ex001-helloWorld.c
@@ -0,0 +1,14 @@
+/*
+This code should return the phrase Hello World!!!, my name is (your name)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	puts("Hello world!!!, my name is Kaio Cesar");
+
+	return 0;
+}
