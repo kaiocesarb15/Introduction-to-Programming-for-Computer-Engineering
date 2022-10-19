7f06fb4a12547d229330683e6d7817f9546205d2
Kaio César Barreto <kaio.barreto@academico.ufpb.br>
2022-10-19 20:36:03 -0300

    Upload ex010-menor.c
    Write a c program that reads two numbers as input and displays the smallest theirs. If the numbers are the same, there will be no difference in which of them
    will be displayed.
    Recommended steps:
        a. Define the two variables with some numeric type.
        b. Read the variables. Remember to always add a prompt (message) before reading to tell the user what to type.
        c. Implement the if-else deviation:
            i. Create a condition that compare the two variables to know which it's bigger.
            ii. Add an output statement to each part of the '"if-else" offset.

---
+++ ex010-menor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+
+    puts("Esse programa diz qual eh o menor valor que voce digitou, por favor, siga as instrucoes abaixo");
+    printf("Digite um numero inteiro para a variavel A: ");
+    scanf("%d", &A);
+    printf("Digite um numero inteiro para a variavel B: ");
+    scanf("%d", &B);
+
+    if (A==B){
+        printf("\nA, B sao iguais e possuem o valor: %d\n", A);
+    }else if (A>B){
+        printf("\nA eh o maior numero e possui o valor: %d\n", A);
+        }else if (B>A){
+            printf("\nB eh o maior numero e possui o valor: %d\n", B);
+            }
+
+    return 0;
+}
+
